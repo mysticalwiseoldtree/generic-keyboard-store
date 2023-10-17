6ca8e214cb766bc42958be75530457784d54c68d
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-10-17 23:42:54 +0800

    Wired frontend with backend

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 
-# --- App Config ---
+### --- App Config --- ###
 app = Flask(__name__)
-
 app.config.from_object(__name__)
 
-CORS(app, resources={r'/*': {'origins': '*'}})
+# Give green lights to the frontend
 CORS(app, resources={r'/*': {'origins': 'http://localhost:8080', 'allow_headers': 'Access-Control-Allow-Origin'}})
 
-# --- Routes ---
+### --- Routes --- ###
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello World!'
+
+@app.route('/sampledata', methods=['GET'])
+def sample_data():
+    return 'This message is from the backend!'
 
 # --- Run App ---
 if __name__ == '__main__':
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
 // Scripts for the component
+const backendMessage = ref("");
+
+onMounted(() => {
+  axios
+    .get("http://127.0.0.1:5000/sampledata")
+    .then((response) => {
+      backendMessage.value = response.data;
+    })
+    .catch((error) => {
+      backendMessage.value = error.message;
+    });
+});
 </script>
 
 <template>
   <div class="container">
     <h1>Home View</h1>
+    <p>message from backend: {{ backendMessage }}</p>
     <button @click="$router.push('/about')">go to About page</button>
   </div>
 </template>
