f239bba31afa30a76f23f4236cd8da0f90b5b7e1
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-10-20 18:14:24 +0800

    Switched to MongoDB Atlas for database

--- backend/modules/product_database.py
+++ backend/modules/product_database.py
@@ -1,6 +1,5 @@
 import pymongo
-
-import dns
+import base64
 import modules.product as product
 
 
@@ -13,9 +12,22 @@ class ProductDatabase:
 
     def __init__(self):
         # Connect to MongoDB
-        self.client = pymongo.MongoClient("127.0.0.1", 27017)
+        self.client = pymongo.MongoClient(
+            base64.b64decode(
+                "bW9uZ29kYitzcnY6Ly93aW5kbnlwOmNoaWNrZW5yaWNlNDIwQHdpbmR5Y2x1c3Rlci56ZXlvZjNtLm1vbmdvZGIubmV0Lz9yZXRyeVdyaXRlcz10cnVlJnc9bWFqb3JpdHk="
+            ).decode("utf-8")
+        )
+        try:
+            self.client.admin.command("ping")
+            print("MongoDB Atlas connection successful.")
+        except Exception as e:
+            print(e)
         self.db = self.client["KayBee"]
-        self.collection = self.db["products"]
+        self.collection = self.db["Products"]
+
+    def get_all_entries(self):
+        # Get all documents from the collection
+        return list(self.collection.find())
 
     def add_entry(self, product: product.Product):
         # Insert a new document into the collection
@@ -30,11 +42,9 @@ class ProductDatabase:
             }
         )
 
-    def read_entry(self, product: product.Product):
+    def read_entry(self, uuid: str):
         # Find a document in the collection by unique_product_id
-        return self.collection.find_one(
-            {"unique_product_id": product.unique_product_id}
-        )
+        return self.collection.find_one({"unique_product_id": uuid})
 
     def update_entry(self, product: product.Product):
         # Update a document in the collection by unique_product_id
@@ -51,6 +61,6 @@ class ProductDatabase:
             },
         )
 
-    def remove_entry(self, product: product.Product):
+    def remove_entry(self, uuid: str):
         # Remove a document from the collection by unique_product_id
-        self.collection.delete_one({"unique_product_id": product.unique_product_id})
+        self.collection.delete_one({"unique_product_id": uuid})
